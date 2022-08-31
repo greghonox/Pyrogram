c84118f86ad901fdb38b86806cce427e7c9ecb0a
gregorio honorato <gregoriohonorato@AZ028.home>
2022-08-30 23:11:01 -0300

    create method for write file

--- main.py
+++ main.py
@@ -5,25 +5,29 @@ data = lambda x: int(datetime.now().strftime(x))
 api_id = 0
 api_hash = ''
 
-phrases_jobs = []
+phrases_jobs = ['python', 'django']
 data_limit = datetime(data('%Y'), data('%m'), 1)
 groups_ids = []
 
 app = Client('search jobs', api_id=api_id, api_hash=api_hash)
 
 async def main():
-    async with app:
-        for group in groups_ids:
-            async for message in app.get_chat_history(group):
-                if message.text is None or message.forward_date is None:
-                    continue
+    with open('/tmp/jobs.txt', 'a') as f:
+        async with app:
+            for group in groups_ids:
+                async for message in app.get_chat_history(group):
+                    if message.text is None or message.forward_date is None:
+                        continue
 
-                if any(phrase in message.text  for phrase in phrases_jobs):
-                    print(message.text+'\n'+message.link)
+                    if any(phrase.lower() in message.text.lower()  for phrase in phrases_jobs):
+                        job = f'{message.forward_date}\n{message.text}\n{message.link}\n'
+                        print(job)
+                        f.write(job)
 
-                if message.forward_date < data_limit:
-                    print(f'end {group}')
-                    break
-                print('-' * 100)
+                    if message.forward_date < data_limit:
+                        print(f'end {group}')
+                        break
+                    print('-' * 100)
+                    f.write('-' * 100 + '\n')
 
 app.run(main())
