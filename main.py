b0c5a9cd132213e63019157878b2e3ce7b1e97d1
Gregorio Honorato <greghono@gmail.com>
2023-12-21 13:26:40 -0300

    feat: change to version with telethon

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 """
     assign for start: https://my.telegram.org/auth?to=apps
 """
-from pyrogram import Client
-from datetime import datetime
+from telethon.sync import TelegramClient
+from re import search
 
-data = lambda x: int(datetime.now().strftime(x))
+
 api_id = 0
-api_hash = ''
+api_hash = ""
 
-phrases_jobs = ['python', 'django', 'back end']
-data_limit = datetime(data('%Y'), data('%m'), 1)
-groups_ids = [-1001052551532, -288176910, -1001052992679, -1001303593538,
-              -1001150558288, -1001259501418, -1001393691685, -1001407720564,
-              -1001436635321, -288176910, -1001715022220]
+groups_search = ["vaga", "emprego", "grup", "job", "talent", "prof", "ti", "it", "t.i"]
+phares_search = ["python"]
+search_p = lambda text: search("|".join(groups_search), text.lower())
+search_j = lambda text: search("|".join(phares_search), text.lower())
+LEN_LENGTH = 200
 
-app = Client('search jobs', api_id=api_id, api_hash=api_hash)
 
-async def get_dialogs():
-    async with app:
-        async for dialog in app.get_dialogs():
-            print(dialog.chat.title or dialog.chat.first_name, dialog.chat.id)
-            
-async def main():
-    with open('/tmp/jobs.txt', 'a') as f:
-        async with app:
-            for group in groups_ids:
-                async for message in app.get_chat_history(group):
-                    if message.text is None or message.forward_date is None:
-                        continue
+def write_file(data):
+    with open("/tmp/jobs.txt", "a") as file:
+        file.write(data)
 
-                    if any(phrase.lower() in message.text.lower()  for phrase in phrases_jobs):
-                        job = f'{message.forward_date}\n{message.text}\n{message.link}\n' + '-' * 100 + '\n'
-                        print(job)
-                        f.write(job)
 
-                    if message.forward_date < data_limit:
-                        print(f'end {group}')
-                        break                    
-# app.run(get_dialogs())
-app.run(main())
+with TelegramClient("anon", api_id, api_hash) as client:
+    for dialog in client.iter_dialogs():
+        if dialog.is_group and search_p(dialog.name):
+            print(f"Group name: {dialog.name}")
+            write_file("-" * LEN_LENGTH + "\n")
+            write_file(f"Group name: {dialog.name}\n")
+            for message in client.iter_messages(dialog.id, limit=1000):
+                if (
+                    message.text is not None
+                    and len(message.text) > 0
+                    and search_j(message.text)
+                ):
+                    write_file(f"{message.date} {message.text}\n")
+                    print(f"Message: {message.text}")
+                    write_file("-" * LEN_LENGTH + "\n")
+
+            print("=" * LEN_LENGTH)
+            write_file("=" * LEN_LENGTH + "\n")
